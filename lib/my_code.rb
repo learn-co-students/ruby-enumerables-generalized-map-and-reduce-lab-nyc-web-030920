4c4cb68058bc8b861d5e7a84931d35ed85b77763
Robert Keller <robert_keller44@icloud.com>
2020-01-15 02:46:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+def map(source_array)
+  #map returns a new array of manipulated data after it has been passed to a block
+  new = []
+  i = 0
+  while i < source_array.length do
+   new << yield(source_array[i])
+    i += 1
+  end
+  new
+end
+
+def reduce(array, starting_value=nil)
+  #reduce returns a value after manipulating data
+ if starting_value
+   sum = starting_value
+   i = 0
+ else
+   sum = array[0]
+   i = 1
+end
+   while i < array.length 
+     sum = yield(sum, array[i])
+     i += 1
+   end
+ sum
+
+end
