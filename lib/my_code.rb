6da91105f771f24d77f2843887facab4eb1fe259
Brett Bedevian <brettbedevian@gmail.com>
2020-02-24 18:06:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# Your Code Here
+def map(array)
+  i = 0 
+  result = []
+  while i < array.length 
+  result << yield(array[i]) 
+  i+=1 
+  end 
+result 
+end 
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  
+  while i < array.length 
+  sum = yield(sum, array[i])
+  i += 1 
+end 
+sum 
+end 
+  
+  
+  
