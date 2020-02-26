750517aa44ecfb51e32a2b0e4ba132b6481617c3
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-26 22:57:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map source
+  ret = []
+  source.each do |val|
+    # yield val
+    ret << (yield val)
+  end
+  return ret
+end
+
+def reduce source, start=nil
+  # either start is index or starting value?
+  if start
+    memo = start
+    i = 0
+  else
+    memo = source[0]
+    i = 1
+  end
+  while i < source.length
+    val = source[i]
+    memo = yield(memo, val)
+    i += 1
+  end
+
+  return memo
+end
